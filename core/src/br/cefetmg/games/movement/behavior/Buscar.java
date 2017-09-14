e5b23386a9feb9fb42f489026857653fc8b3ecc5
Luizaas <luizanjosouza@gmail.com>
2017-09-14 11:32:24 -0300

    buscar

--- core/src/br/cefetmg/games/movement/behavior/Buscar.java
+++ core/src/br/cefetmg/games/movement/behavior/Buscar.java
@@ -4,6 +4,7 @@ import br.cefetmg.games.movement.AlgoritmoMovimentacao;
 import br.cefetmg.games.movement.Direcionamento;
 import br.cefetmg.games.movement.Pose;
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.math.Vector3;
 
 /**
  * Guia o agente na direção do alvo.
@@ -26,7 +27,13 @@ public class Buscar extends AlgoritmoMovimentacao {
     @Override
     public Direcionamento guiar(Pose agente) {
         Direcionamento output = new Direcionamento();
-
+        Vector3 aux = alvo . getObjetivo();
+        
+        output.velocidade = aux.sub(agente.posicao);
+        output.velocidade.nor();
+        output.velocidade.scl(maxVelocidade);
+        agente.olharNaDirecaoDaVelocidade(output.velocidade);
+        output.rotacao=0;
         // calcula que direção tomar (configura um objeto Direcionamento 
         // e o retorna)
         // ...
